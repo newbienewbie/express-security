7912ea88f881c8c5d9dbab0b85102aca72e36e27
newbienewbie <webbot@sina.com>
2016-12-04 11:16:23 +0800

    在兼容原调用方式的前提下，重写新的失败消息生成函数
    现在除了可以直接传递一个URL字符串，还支持以传递一个对象的方式进行调用。
    如果传递的对象中还含有responseMessage属性，则直接返回responseMessage作为失败消息

--- test/msg-generator.test.js
+++ test/msg-generator.test.js
@@ -4,14 +4,14 @@ const msgGenerator=require('../lib/msg-generator');
 
 describe('测试 msg-generator.js',function(){
 
-    describe('#generateLoginFailMsg()',function(){
+    describe('#generateLoginFailMsgWithParams()',function(){
 
         let msg="您尚未登陆，即将为您转向登陆";
         let timeout=1;
         let url='/';
 
         it('默认情况',function(){
-            const failMsg=msgGenerator.generateLoginFailMsg();
+            const failMsg=msgGenerator.generateLoginFailMsgWithParams();
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
         
@@ -19,7 +19,7 @@ describe('测试 msg-generator.js',function(){
             msg="您尚未登陆";
             timeout='2';
             url="/sss"
-            const failMsg=msgGenerator.generateLoginFailMsg(url,msg,timeout);
+            const failMsg=msgGenerator.generateLoginFailMsgWithParams(url,msg,timeout);
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
     });
@@ -31,22 +31,28 @@ describe('测试 msg-generator.js',function(){
             let {url,msg,timeout}=obj;
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
-        it('定制参数情况',function(){
+        it('定制参数情况：无responseMessage的情况',function(){
             const obj={url:"/ss",msg:'helloworld',timeout:21};
             const failMsg=msgGenerator.generateLoginFailMsgWithObj(obj);
             let {url,msg,timeout}=obj;
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
+        it('定制参数情况：有responseMessage的情况',function(){
+            const obj={url:"/ss",msg:'helloworld',timeout:21,responseMessage:'double kill'};
+            const failMsg=msgGenerator.generateLoginFailMsgWithObj(obj);
+            let {url,msg,timeout}=obj;
+            assert.equal(failMsg,`double kill`);
+        });
     });
 
-    describe('#generateRoleFailMsg()',function(){
+    describe('#generateRoleFailMsgWithParams()',function(){
 
         let msg="您无此权限，即将为您重定向";
         let timeout=1;
         let url='/';
 
         it('默认情况',function(){
-            const failMsg=msgGenerator.generateRoleFailMsg();
+            const failMsg=msgGenerator.generateRoleFailMsgWithParams();
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
         
@@ -54,7 +60,7 @@ describe('测试 msg-generator.js',function(){
             msg="您尚未此权限";
             timeout='2';
             url="/sss"
-            const failMsg=msgGenerator.generateRoleFailMsg(url,msg,timeout);
+            const failMsg=msgGenerator.generateRoleFailMsgWithParams(url,msg,timeout);
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
     });
@@ -67,11 +73,17 @@ describe('测试 msg-generator.js',function(){
             let {url,msg,timeout}=obj;
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
         });
-        it('定制参数情况',function(){
+        it('定制参数情况:无responseMessage的情况',function(){
             const obj={url:"/ss",msg:'helloworld',timeout:21};
             const failMsg=msgGenerator.generateRoleFailMsgWithObj(obj);
             let {url,msg,timeout}=obj;
             assert.equal(failMsg,`${msg}<meta http-equiv="refresh" content="${timeout};url='${url}'"/>`);
+        });
+        it('定制参数情况:有responseMessage的情况',function(){
+            const obj={url:"/ss",msg:'helloworld',timeout:21,responseMessage:"hollyshit"};
+            const failMsg=msgGenerator.generateRoleFailMsgWithObj(obj);
+            let {url,msg,timeout}=obj;
+            assert.equal(failMsg,`hollyshit`);
         });
     });
     

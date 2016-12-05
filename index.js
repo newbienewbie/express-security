192e3cb8f80b646a929f16cd49df20190c56e3cc
newbienewbie <webbot@sina.com>
2016-12-05 16:07:42 +0800

    bugfix：修复未导出AuthInterceptor的错误

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const AuthenticationInterceptor=require('./lib/authentication-interceptor');
 const AuthorizationInterceptor=require('./lib/authorization-interceptor');
 const AuthorizationChecker=require('./lib/authorization-checker');
+const AuthInterceptor=require('./lib/auth-auth-interceptor');
 
 
 module.exports={AuthInterceptor,AuthenticationInterceptor,AuthorizationInterceptor,AuthorizationChecker};
